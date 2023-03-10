2d25d25ee5d23fc0c8017077754b86cd86b87158
ncp2306 <91904251+ncp2306@users.noreply.github.com>
2023-03-10 11:22:52 -0600

    Update RNA_celltype_annotation.R

--- code/RNA+TCR+BCR_preprocessing/RNA_celltype_annotation.R
+++ code/RNA+TCR+BCR_preprocessing/RNA_celltype_annotation.R
@@ -9,7 +9,7 @@
 #
 # Date: 08-30-2022
 # Written by: Natalie Piehl
-# Summary: Run supervised clustering
+# Summary: Identify cell types using CITEseq reference based mapping
 #
 #-------------------------------------------------------------------------------
 # Initialization
@@ -23,11 +23,9 @@ suppressMessages({
 })
 
 # Initialize input parameters
-seurat_object <- "/projects/b1169/projects/AD_APOE/results/seurat/clustering_no1086/out_NP_08-24-2022/s_integrated"
-# seurat_object <- "/projects/b1169/nat/CSF_immunity_age/results/tcr_cleanup/main/out/s_tcrclean"
-ref_atlast_path <- "/projects/b1169/nat/CSF_immunity_age/data/celltype_atlas/pbmc_multimodal.h5seurat"
-output_dir <- "/projects/b1169/projects/AD_APOE/results/seurat/supervised_clustering/out_NP_08-30-2022/"
-# test_seurat_object <- paste0(output_dir, "s_test.rds")
+seurat_object <- "/path/to/integrated/seurat/object"
+ref_atlast_path <- "/path/to/pbmc_CITEseq_ref"
+output_dir <- "/path/to/output/folder"
 dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 
 #------------------------------------------------------------------------------
@@ -37,15 +35,10 @@ dir.create(output_dir, showWarnings = FALSE, recursive = TRUE)
 load(seurat_object)
 s
 
-# Idents(s) <- "orig.ident"
-# samples_to_use <- unique(s[["orig.ident"]])[1:10,1]
-# s <- subset(s, orig.ident %in% samples_to_use)
-# s <- subset(s, downsample = 500)
-
 # Load in reference
 reference <- LoadH5Seurat(ref_atlast_path)
 
-# Run standard normalization
+# Set assay to SCT
 DefaultAssay(object = s) <- "SCT"
 
 # Find transfer anchors
@@ -75,12 +68,13 @@ s
 # Save updated object
 save(s, file = paste0(output_dir, "s_sup_clustering"))
 
-# Visualize results
+# Order cell type colors
 celltype_colors <- read.csv(celltype_colors_path)
-celltype_colors[which(celltype_colors$predicted.celltype.l2 == "NK CD56bright"), "predicted.celltype.l2"] <- "NK_CD56bright"
 celltype_colors <- celltype_colors[which(celltype_colors$predicted.celltype.l2 %in% unique(s[["predicted.celltype.l2"]])[,1]),]
 s$predicted.celltype.l2 <- factor(s$predicted.celltype.l2,
                                   levels = celltype_colors$predicted.celltype.l2)
+
+# Generate original UMAP plots
 p1 = DimPlot(s, reduction = "umap", group.by = "predicted.celltype.l1", label = TRUE, label.size = 3, repel = TRUE)
 p2 = DimPlot(s, reduction = "umap", group.by = "predicted.celltype.l2",
              label = TRUE, label.size = 3 ,repel = TRUE,
@@ -88,8 +82,7 @@ p2 = DimPlot(s, reduction = "umap", group.by = "predicted.celltype.l2",
 set_panel_size(p1, file = paste0(output_dir, "og_umap_L1.pdf"), width = unit(5, "in"), height = unit(4, "in"))
 set_panel_size(p2, file = paste0(output_dir, "og_umap_L2.pdf"), width = unit(5, "in"), height = unit(4, "in"))
 
-
-
+# Generate ref UMAP plot
 p2 = DimPlot(s, reduction = "ref.umap", group.by = "predicted.celltype.l2",
              label = TRUE, label.size = 3 ,repel = TRUE,
              cols = celltype_colors$color)
